afe2dc347ef55687ada6ae5ec08d8c2b47ef809a
Paweł Zaręba <pzareba22@gmail.com>
2022-02-09 20:04:43 +0100

    Started with the initial layout

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="main">
+    <header class="mainHeader">
+      <h1>Vordle</h1>
+    </header>
+    <div class="lettergrid"></div>
+    <div class="keyboard">
+      <div class="keybaordRow"></div>
+      <div class="keybaordRow"></div>
+      <div class="keybaordRow"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style  scoped>
+*{
+    font-family: sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+.main {
+    width: 45vw;
+    height: 100vh;
+    margin: 0 auto;
+    background-color: #f6f6f6;
+    display: flex;
+    flex-direction: column;
+}
+
+.mainHeader {
+    background-color: #BFEE9B;
+    color: #fff;
+    padding: 2rem 0;
+    text-align: center;
+    font-size: x-large;
+}
+
+
+</style>
