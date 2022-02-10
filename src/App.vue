71f9cbddc8af3eff1d96cfebde50f3549f3cad2a
Paweł Zaręba <pzareba22@gmail.com>
2022-02-10 16:02:04 +0100

    night of hacking

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <div class="main">
-    <header class="mainHeader">
-      <h1>Vordle</h1>
-    </header>
-    <div class="lettergrid"></div>
-    <div class="keyboard">
-      <div class="keybaordRow"></div>
-      <div class="keybaordRow"></div>
-      <div class="keybaordRow"></div>
+    <div class="main">
+        <header class="mainHeader">
+            <h1>Vordle</h1>
+        </header>
+        <div class="letterGrid">
+            <div class="letterRow" v-for="(row, ind) in letters" :key="ind">
+                <LetterSquare
+                    v-for="(letter, ind2) in row"
+                    :key="ind2"
+                    :letter="letter"
+                />
+            </div>
+        </div>
+        <Keyboard
+            @click="updateLetter"
+            :enabled="currentCol < 6"
+            :validKeys="validKeys"
+        />
     </div>
-
-  </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import LetterSquare from "./components/LetterSquare.vue";
+import Keyboard from "./components/Keyboard.vue";
+export default {
+    data() {
+        return {
+            letters: Array.from(Array(5), () => new Array(6).fill(" ")),
+            currentRow: 0,
+            currentCol: 0,
+            validKeys: ["a"],
+            word: "",
+        };
+    },
+    components: {
+        LetterSquare,
+        Keyboard,
+    },
+    methods: {
+        updateLetter(letter) {
+            if (letter === "del") {
+                if (this.currentCol > 0) {
+                    this.letters[this.currentRow][this.currentCol - 1] = " ";
+                    this.currentCol -= 1;
+                }
+            } else {
+                if (this.currentCol < 6) {
+                    this.letters[this.currentRow][this.currentCol] = letter;
+                    this.currentCol += 1;
+                }
+            }
+        },
+    },
+};
 </script>
 
-<style  scoped>
-*{
+<style scoped>
+* {
     font-family: sans-serif;
     box-sizing: border-box;
     margin: 0;
@@ -28,21 +64,36 @@
 }
 
 .main {
-    width: 45vw;
+    width: 55vw;
     height: 100vh;
     margin: 0 auto;
-    background-color: #f6f6f6;
+    background-color: #fdfdfd;
     display: flex;
     flex-direction: column;
+    align-items: center;
 }
 
 .mainHeader {
-    background-color: #BFEE9B;
+    background-color: #bfee9b;
     color: #fff;
     padding: 2rem 0;
     text-align: center;
     font-size: x-large;
+    letter-spacing: 0.3rem;
+    width: 100%;
 }
 
+.letterGrid {
+    flex-grow: 8;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+}
 
+.letterRow {
+    display: flex;
+    gap: 0.5em;
+}
 </style>
